cf9f9c1247042d3e2569d71403e89c304d636a24
Marcos Quaresma <quaresmamarcoslol@gmail.com>
2026-08-03 21:14:42 -0300

    # Fundamentos Python - Primeiras aulas

---
+++ Main.py
@@ -0,0 +1,39 @@
+# Talvez 30° programa em Python, justificativa pra aprendizagem... hahaha.
+print ("I like Sushi!")
+print ("Sushi só é bom cru!")
+
+# Variavel = É um container para um valor (String, Inteitros, Float, Boolean)
+#           Uma variavel se comporta com se fosse o valor que contem.
+
+# Strings
+primeiro_nome = "Marcos"
+comida = "Pizza"
+email = "quaresmamarcos@gmail.com"
+
+print(f"Hello {primeiro_nome}!")
+print(f"você gostaria de comer {comida}?")
+print(f"Seu email é: {email} correto?")
+
+# Inteiros
+idade = 25
+quantidade = 3
+numero_de_estudantes = 10
+
+print(f"Sua iade é {idade} anos.")
+print(f"Você esta comprando {quantidade} itens.")
+print(f"Sua classe tem {numero_de_estudantes} alunos.")
+
+# Float é um número mas contem uma parte decimal, ex: 3.2
+preco = 10.99
+media = 8.5
+distancia = 3.48
+
+print(f"Preço é {preco}!")
+print(f"Sua média anual é: {media}")
+print(f"Sua distancia ao trabalho é de {distancia}km.")
+
+# Um Boolean é V ou F, verdadeiro ou falço, "True" or "False" - Primeira letra em Maiúsculo sempre.
+#                   Se usa mais internamente, com If e Else, não ficam "expostos"
+e_aluno = True
+
+print(f"Você é um aluno?: {e_aluno}") # Apenas demonstrativo Boolean.
