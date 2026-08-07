9d2cb506ad577333a7f2545dc805e79d367199ba
Marcos Quaresma <quaresmamarcoslol@gmail.com>
2026-08-07 05:29:37 -0300

    exercicio_03.py

--- exercicio_03.py
+++ exercicio_03.py
@@ -1,1 +1,12 @@
 # Programa de carrinho de compras.
+item = input ("Qual item você gostaria de comprar? ")
+price = float(input ("Qual é o preço? "))
+quantidade = int(input ("Quantos você gostaria? "))
+
+total = price * quantidade
+
+desconto = total * 0.10
+Ftotal = total - desconto
+
+print(f"Voce comprou {quantidade} x {item} Total: R$ {round(total), 2} \n Com 10% de desconto: ")
+print(f"Total a ser pago: R$ {round(Ftotal), 2}")
