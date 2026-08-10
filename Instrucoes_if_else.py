498f680c72fbcb41ac86038bbf9753b0bae33a2b
Marcos Quaresma <quaresmamarcoslol@gmail.com>
2026-08-09 23:43:25 -0300

    Começando_os_estudos_09082026

---
+++ Instrucoes_if_else.py
@@ -0,0 +1,4 @@
+# if = é usada para executar um código somente se uma determinada condição que definimos for True
+#      caso contrário iremos fazer Else, é uma forma básica de tomar decisão, baseada em "Ture" e "False"
+
+idade = int(input("Qual a sua idade: "))
