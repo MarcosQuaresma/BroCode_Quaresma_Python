3f07060430f4a838f21f933e0fc25de315898233
Marcos Quaresma <quaresmamarcoslol@gmail.com>
2026-08-13 12:46:04 -0300

    metodos_uteis_para_strings

--- expressoes_condicionais.py
+++ expressoes_condicionais.py
@@ -3,8 +3,19 @@
 #                   Fórmula: X if condition else Y
 #                    Retorna X se a condição for verdadeira(True), Se não(Else) retorna Y.
 
-num = 8
+num = 5
+a = -1
+b = 1
+idade = 17
+temperatura = 20
+cargo_funcionario = "convidado"
 
 # print(f"o número {num} é par" if num % 2 == 0 else f"O número {num} impar")
-# print("Positivo"if num > 0  else "Negativo")
-resultado =
+# print("Positivo" if num > 0  else "Negativo")
+# maior_num = a if a > b else b
+# menor_num = a if a < b else b
+# estatus = "Adulto" if idade >= 18 else "Criança"
+# agua = "Frio" if temperatura < 20 else "Quente"
+nivel_de_acesso = "acesso total" if cargo_funcionario == "admin" else "acesso limitado"
+
+print (nivel_de_acesso)
---
+++ metodos_uteis_srt.py
@@ -0,0 +1,23 @@
+# Métodos úteis para strings
+
+# nome = input("Digite seu nome completo: ")
+telefone = input("Digite seu telefone: ")
+
+# resultado = len(nome) # ele retorna um inteiro(int) com a quantidade
+                        # de caracteres tem ums "srt" com espaços e tudo.
+# .find() ele retorna a primeira ocorrencia de um determinado caractere.
+# resultado = nome.find("M") # no nome "Marcos Pedrosa Quaresma" é no índice 6
+                             # porque sempre começamos com ZERO. indice "M" = 0
+# resultado = nome.rfind("m") # aqui ele ira procurar a ÚLTIMA vez que ocorreu o
+                              # índice "m" minúsculo.
+# ~~ caso não encontre nada retornara "-1" no terminal.
+# resultado = nome.capitalize() # Primeira letra da frase ficara em Maiúsculo.
+# resultado = nome.upper() #Toda a "srt" em MAIÚSCULO.
+# resultado = nome.lower() #Tudo em minúsculo.
+# resultado = nome.isdigit() # Só retorna True se for apenas Dígitos, else = False
+# resultado = nome.isalpha() # só retorna True se conter apenas caractere alfabetico
+                             # caso tenha um " " vazio ou algo que não seja somente
+                             # "srt" ele devolve False
+# resultado = telefone.count("-") # ele conta quantos caracteres existe na srt
+
+print(resultado)
