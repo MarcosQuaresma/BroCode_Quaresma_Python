37eab42256d9660fd020888683a99fed5fe30f8a
Marcos Quaresma <quaresmamarcoslol@gmail.com>
2026-08-16 02:12:30 -0300

    ecercicio_indexacao_str

---
+++ exercicio_07_indexacao_de_str.py
@@ -0,0 +1,8 @@
+
+num_credito = "1234-5678-9012-3456"
+
+ultimos_digitos = num_credito [-4: ]
+num_credito_reverso = num_credito [::-1 ]
+
+print(num_credito_reverso)
+# print(f"xxx-xxx-xxx-{ultimos_digitos}")
--- indexacao_de_str.py
+++ indexacao_de_str.py
@@ -4,8 +4,16 @@
 
 num_credito = "1234-5678-9012-3456"
 
-print(num_credito[0])  # se eu quiser o primero caractere da Str.
-print(num_credito[1])  # se eu quiser o segundo caractere da Str.
-print(num_credito[4])  # se eu quiser o quinto caractere da Str.
+print(num_credito[0])  # se eu quiser o primero caractere da Str. (start)
+print(num_credito[1])  # se eu quiser o segundo caractere da Str. (start)
+print(num_credito[4])  # se eu quiser o quinto caractere da Str. (start)
 
-print(num_credito[0:4])  # se eu quiser-mos os 4 primeiros caractere da Str.
+print(num_credito[0:4])  # se eu quiser-mos os 4 primeiros caractere da Str. (start : end)
+print(num_credito[5:9])  # se eu quiser mostar do 5° ao 9° caractere na Str. (start : end)
+print(num_credito[7:])   # se eu quiser que mostre todos os caracteres na Str depois do 7° (start : end)
+
+print(num_credito[-1])   # se eu quiser que mostre o ultimo caracteres na Str. ( : end)
+print(num_credito[-2])   # se eu quiser que mostre o penultimo caracteres na Str. (: end)
+
+print(num_credito[::2])  # Isso imprimirá com um intervalo de 2 caracteres. ( :: step)
+print(num_credito[::3])  # Isso imprimirá com um intervalo de 3 caracteres. ( :: step)
