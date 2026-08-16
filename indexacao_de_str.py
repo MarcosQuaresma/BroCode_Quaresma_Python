95d3cea91b8f80eb591461e87c32ecc7e42d43c7
Marcos Quaresma <quaresmamarcoslol@gmail.com>
2026-08-15 23:59:53 -0300

    alteracoes_e_indexacao_de_str's

---
+++ indexacao_de_str.py
@@ -0,0 +1,11 @@
+#                 Indexação de String(str)
+# Indexação = permite acessar elementos de uma sequencia usando um conjunto de colchetes [] (operador de indexação)
+#             [start : end : step]
+
+num_credito = "1234-5678-9012-3456"
+
+print(num_credito[0])  # se eu quiser o primero caractere da Str.
+print(num_credito[1])  # se eu quiser o segundo caractere da Str.
+print(num_credito[4])  # se eu quiser o quinto caractere da Str.
+
+print(num_credito[0:4])  # se eu quiser-mos os 4 primeiros caractere da Str.
