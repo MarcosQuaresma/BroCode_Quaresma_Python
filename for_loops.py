60fbb5902f0bab17fb3cf055bce2c02516ddc6d1
Marcos Quaresma <quaresmamarcoslol@gmail.com>
2026-08-20 14:11:45 +0100

    atualizacao_for_loops

--- for_loops.py
+++ for_loops.py
@@ -4,7 +4,14 @@
 # for x in reversed(range(1, 11, )): # estrututra simples de "for loop"
 #     print(x)
 
-cartao_de_credito = "1234-5678-9012-3456"
+# cartao_de_credito = "1234-5678-9012-3456"
+# for  x in (cartao_de_credito):
+#    print(x)
 
-for  x in cartao_de_credito:
-    print(x)
+for x in range(1, 21):
+    if x == 13:
+        #~~ continue # palavra chave para "ignorar" algo
+        break #~~ nesse caso se houver a identificação positiva para uma linha de código designada no "if"
+              #~~ ele para a continuidade ou sequencia. "sairemos do loop"
+    else:
+        print(x)
